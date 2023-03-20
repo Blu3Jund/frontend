1b15e720b58a2113d8fbe9db219586fea57c9cc3
benjaminshawki <benjaminshawi@gamil.com>
2023-03-20 01:56:09 +0100

    feat: grid home and pagination

--- src/app/products/horizontal-scroll-bar/horizontal-scroll-bar.component.ts
+++ src/app/products/horizontal-scroll-bar/horizontal-scroll-bar.component.ts
@@ -1,4 +1,36 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+// import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+// import { Product } from '../../shared/models/product.model';
+//
+// @Component({
+//   selector: 'app-horizontal-scroll-bar',
+//   templateUrl: './horizontal-scroll-bar.component.html',
+//   styleUrls: ['./horizontal-scroll-bar.component.css'],
+// })
+// export class HorizontalScrollBarComponent {
+//   @Input() products: Product[];
+//
+//   private container: HTMLElement;
+//
+//   ngOnInit() {
+//     this.container = document.querySelector('.container');
+//   }
+//
+//   scrollLeft() {
+//     this.container.scrollTo({
+//       left: this.container.scrollLeft - this.container.offsetWidth,
+//       behavior: 'smooth',
+//     });
+//   }
+//
+//   scrollRight() {
+//     this.container.scrollTo({
+//       left: this.container.scrollLeft + this.container.offsetWidth,
+//       behavior: 'smooth',
+//     });
+//   }
+// }
+
+import { Component, Input } from '@angular/core';
 import { Product } from '../../shared/models/product.model';
 
 @Component({
@@ -8,4 +40,29 @@ import { Product } from '../../shared/models/product.model';
 })
 export class HorizontalScrollBarComponent {
   @Input() products: Product[];
+  // products: Product[] = [];
+  currentPage: number = 1;
+  pageSize: number = 25;
+
+  get startIndex() {
+    return (this.currentPage - 1) * this.pageSize;
+  }
+
+  get endIndex() {
+    return this.currentPage * this.pageSize;
+  }
+
+  get totalPages() {
+    return Math.ceil(this.products.length / this.pageSize);
+  }
+
+  constructor() {}
+
+  prevPage() {
+    this.currentPage--;
+  }
+
+  nextPage() {
+    this.currentPage++;
+  }
 }
